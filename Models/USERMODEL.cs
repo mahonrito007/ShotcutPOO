6a5f17440a3ab169add293af5f9aa25622ce2501
Mahonrito007 <Mahonrito007@gmail.com>
2025-04-11 18:46:39 -0600

    cambio 1
    Añadidas clases USERMODEL y usuario en ShotcutPOO.Models
    
    Se ha añadido #una nueva clase `USERMODEL` en el archivo `USERMODEL.cs` dentro del espacio de nombres `ShotcutPOO.Models`. Esta clase contiene las propiedades `ID`, `Nombre`, `Apellido` y `Correo`.
    
    Se ha añadido una nueva clase `usuario` en el archivo `usuario.cs` dentro del espacio de nombres `ShotcutPOO.Models`. Esta clase contiene los campos `nombre` y `apellido`.

---
+++ Models/USERMODEL.cs
@@ -0,0 +1,10 @@
+﻿namespace ShotcutPOO.Models
+{
+    public class USERMODEL
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+    }
+}
