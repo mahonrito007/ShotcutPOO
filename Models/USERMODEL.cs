dcaac0ae939183c11081feaf498dd499f0e9cbf9
SahidGuardado <sahidguardado@gmail.com>
2025-04-14 08:26:58 -0600

    Add employee management actions and UI updates
    - Implemented new action methods `Agregar`, `Eliminar`, and `Editar` in `HomeController` for employee management.
    - Enhanced `Index.cshtml` layout with a logo and buttons for the new actions.
    - Added `Logo Shortcut.png` for use in the updated UI.
    
    Add employee management features and database integration
    
    - Updated HomeController to connect to SQL Server and query employee data using Dapper.
    - Renamed USERMODEL to Empleados and updated its properties to reflect employee details.
    - Removed the obsolete usuario model.
    - Added Dapper and Microsoft.Data.SqlClient package references in the project file.
    - Modified Index view to display employee data in a structured table format.

--- Models/USERMODEL.cs
+++ Models/USERMODEL.cs
@@ -1,10 +1,20 @@
-﻿namespace ShotcutPOO.Models
+﻿using System.Reflection;
+
+namespace ShotcutPOO.Models
 {
-    public class USERMODEL
+ 
+
+    public class Empleados
     {
         public int ID { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Correo { get; set; }
+        public string nombre_completo { get; set; }= string.Empty;
+        public string puesto { get; set; } = string.Empty;
+        public double sueldo_base { get; set; }
+        public double sueldo_total { get; set; }
+        public int horario { get; set; }
+        public int jefe_inmediato_ID { get; set; }
+
     }
+
+ 
 }
